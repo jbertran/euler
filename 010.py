7cd4aed19f057e25a3b38935e1bd0e46f7837556
Jordi BERTRAN DE BALANDA (swhintern) <j.debalanda@gmail.com>
2016-10-24 03:54:23 +0200

    010 done, 011 wip

--- 010.py
+++ 010.py
@@ -1,17 +1,25 @@
 import itertools as it
 
-def erat2(n):
+def erat2():
     D = {}
     i = 1
-    for q in itertools.islice(itertools.count(3), 0, None, 2):
+    l = [2]
+    for q in it.islice(it.count(3), 0, None, 2):
         p = D.pop(q, None)
         if p is None:
             D[q*q] = q
             i += 1
-            if i == n:
-                return q
+            if q > 2e6:
+                return l
+            l.append(q)
         else:
             x = p + q
             while x in D or not (x&1):
                 x += p
             D[x] = p
+
+res = erat2()
+
+print(res[:10])
+
+print(sum(res))
