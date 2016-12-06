a7ae60fb45cc97ec95db4513d89bed9fc6ac98a6
Jordi BERTRAN DE BALANDA (swhintern) <j.debalanda@gmail.com>
2016-12-06 18:58:31 +0100

    012 wip

--- 012.py
+++ 012.py
@@ -1,4 +1,95 @@
-# this is terrible
+import math
+
+# Ugly. Gotta sieve prime dividers and combine them instead
+
+dividers = {}
+
+max_divs = 0
+tr = 0
+next_add = 1
+
+while True:
+    tr += next_add
+
+    tr_divs = [1]
+    it = math.ceil(math.sqrt(tr))
+    while it > 1:
+        if tr % it == 0:
+            if dividers.get(it):
+                for div in dividers[it]:
+                    if div not in tr_divs:
+                        tr_divs.append(div)
+                break
+            else:
+                tr_divs.append(it)
+        it -= 1
+
+    dividers[tr] = tr_divs
+
+    if len(dividers[tr]) >= 500:
+        break
+    if len(dividers[tr]) > max_divs:
+        max_divs = len(dividers[tr])
+
+    next_add += 1
+    if next_add % 1000 == 0:
+        print(next_add, tr, max_divs)
+
+print(tr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 cur_step = 2
 triangular = 1
